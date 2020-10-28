b4d38f589ab603dc50988dd7c304b0a1f555253a
geos-pc-86 <kozhemyakina_alice>
2020-10-28 16:46:19 +0700

    manage msg

--- client/src/components/ChatRoom.vue
+++ client/src/components/ChatRoom.vue
@@ -1,27 +1,27 @@
 <template>
-<div class="chat-room">
+<div class="chat-room" >
   <div class="messages">
     <div class="message" :style="message.username === myUsername ? 'justify-content: flex-end' : 'justify-content: flex-start'" v-for="message in messages" :key="message.index">
-      <div class="message-block">
+      <div class="message-block" @click="showManage" v-click-outside="hideManageMsg">
         <div class="username">{{message.username === myUsername ? 'You' : message.username}}</div>
         <div class="message-block__info">
           <div class="text">{{message.msg}}</div>
           <div class="time">{{ message.time }}</div>
         </div>
-        
       </div>
     </div>
   </div>
   <div class="writing">
-    <form @submit.prevent="Send()">
+    <form @submit.prevent="Send">
       <textarea
           v-model.trim="msg"
           placeholder="Type a message..."
+          @keyup.enter.exact="Send"
       />
-      <input type="submit" tabindex="-1" hidden />
       <v-btn type='submit' :disabled="!msg">Send</v-btn>
     </form>
   </div>
+  <div class="manage-message">XNJNJ</div>
 </div>
 </template>
 
@@ -31,7 +31,8 @@ name: "ChatRoom",
   props: ['messages', 'myUsername'],
   data() {
     return {
-      msg: ''
+      msg: '',
+      isManageShow: false
     }
   },
   methods: {
@@ -41,16 +42,48 @@ name: "ChatRoom",
       }
       this.$emit('sendMessage', this.msg)
       this.msg = ''
+    },
+    showManage(e) {
+      let msg = document.getElementsByClassName('manage-message')[0]
+      if (!this.isManageShow) {
+        msg.style.display = 'block'
+        msg.style.left = e.pageX + 'px'
+        msg.style.top = e.pageY + 'px'
+        this.isManageShow = true
+      } else {
+        msg.style.display = 'none'
+        this.isManageShow = false
+      }
+    },
+    hideManageMsg() {
+      document.getElementsByClassName('manage-message')[0].style.display = 'none'
+      this.isManageShow = false
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
+
+::-webkit-scrollbar {
+  width: 4px;
+}
+
+::-webkit-scrollbar-track {
+  border-radius: 5px;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #f5f5f5;
+  border-radius: 5px;
+}
+
 .messages {
   height: 70.5vh;
   overflow-y: auto;
   padding: 12px;
+  width: 1200px;
+  margin: 0 auto;
 }
 .message {
   display: flex;
@@ -66,6 +99,11 @@ name: "ChatRoom",
     border-radius: 5px;
     width: fit-content;
     max-width: 360px;
+    cursor: pointer;
+    transition: .2s;
+    &:hover {
+      background: #b2cece;
+    }
     &__info {
       width: fit-content;
       display: flex;
@@ -76,6 +114,9 @@ name: "ChatRoom",
         margin-left: 8px;
         opacity: 50%;
         font-weight: 600;
+      }
+      .text {
+        word-break: break-word;
       }
     }
   }
@@ -95,12 +136,18 @@ name: "ChatRoom",
     textarea {
       outline: none;
       width: 580px;
+      height: 80px;
       color: aliceblue;
-      margin-right: 8px;
+      margin-right: 24px;
       font-weight: 300;
       resize: none;
     }
   }
+}
+.manage-message {
+  display: none;
+  position: absolute;
+  z-index: 100;
 }
 
 </style>
